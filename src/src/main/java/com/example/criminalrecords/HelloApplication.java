0d0605a71919b86b428b4fd065a1a8fa335717c2
Tatjana Russo <tatjana.russo@edu.tbz.ch>
2021-10-02 19:16:38 +0200

    login-site added

--- src/src/main/java/com/example/criminalrecords/HelloApplication.java
+++ src/src/main/java/com/example/criminalrecords/HelloApplication.java
@@ -2,7 +2,17 @@ package com.example.criminalrecords;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +21,45 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Login");
+
+        //Grid setup
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        //create scene
+        Scene scene = new Scene(grid, 800, 775);
+
+        //Input fields
+        Text scenetitle = new Text("Login:");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
+        grid.add(scenetitle, 0,0,2,1);
+
+        //Input user
+        Label user = new Label("User:");
+        grid.add(user, 0,1);
+        TextField userfield = new TextField();
+        userfield.setPromptText("Enter\\your\\user");
+        grid.add(userfield,1,1);
+
+        //Input target
+        Label password = new Label("Password:");
+        grid.add(password, 0, 2);
+        TextField passwordfield = new TextField();
+        passwordfield.setPromptText("password");
+        grid.add(passwordfield, 1,2);
+
+        //Button Login
+        Button btn = new Button("Login");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 2, 9);
+
+
         stage.setScene(scene);
         stage.show();
     }
