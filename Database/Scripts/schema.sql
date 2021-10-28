2a46040632332d8dc3276972fb4d047a130c73cc
Lorin Fankhauser1 <lorin.fankhauser@unic.com>
2021-10-28 15:23:23 +0200

    Added Data to Database

--- Database/Scripts/schema.sql
+++ Database/Scripts/schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE address(
 CREATE TABLE crime(
     crime_id int NOT NULL AUTO_INCREMENT,
     crime varchar(255) NOT NULL,
-    sentence varchar(255) NOT NULL,
+    sentence int NOT NULL,
     victim varchar(255) NULL,
     location varchar(255) NULL,
     weapon varchar(255) NULL,
@@ -44,35 +44,10 @@ CREATE TABLE criminals(
     weight int NOT NULL,
     PRIMARY KEY (criminal_id),
     FOREIGN KEY (address_id) REFERENCES address(address_id),
-    FOREIGN KEY (crime_id) REFERENCES crime(crime_id),
+    /*FOREIGN KEY (crime_id) REFERENCES crime(crime_id),*/
     FOREIGN KEY (ethnicity_id) REFERENCES ethnicity(ethnicity_id)
 );
 
 
-INSERT INTO `ethnicity` (`ethnicity`)
-VALUES
-    ("white");
 
-INSERT INTO `plz` (`plz`,`ort`)
-VALUES
-    (8804,"AU");
-
-INSERT INTO `address` (`plz`,`address`)
-VALUES
-    (8804,"Alte Landstrasse 120b");
-
-
-
-INSERT INTO `crime` (`crime`,`sentence`,`victim`,`location`,`weapon`)
-VALUES
-    ("Murder","Death","Jan Loeliger","TBZ","Laptop");
-
-
-INSERT INTO `criminals` (`address_id`,`crime_id`,`ethnicity_id`,`first_name`,`last_name`,`age`,`height`,`weight`)
-VALUES
-  (1,1,1,"Aline","Nicholson",68,171,121),
-  (1,1,1,"Audra","Hendricks",38,156,50),
-  (1,1,1,"Kermit","Kent",69,165,60),
-  (1,1,1,"Steven","Myers",57,180,140),
-  (1,1,1,"Lillian","Bentley",51,181,133);
 
